c6871f756fb59c2cfd90e1572ebd6b0e4706cd0b
George Higbie <gehigbie@gmail.com>
2017-08-13 18:37:44 -0600

    Configure MinMaxController

---
+++ controllers/MinMaxController.js
@@ -0,0 +1,15 @@
+(function(){
+   var MinMaxController = function($scope, $http){
+       $scope.formModel = {};
+
+       $scope.onSubmit = function(){
+           console.log("Hey I/'m Submitted!");
+           console.log($scope.formModel);
+       };
+
+       MinMaxController.$inject = ['$scope'];
+
+       angular.module('minmax')
+           .controller('MinMaxController', MinMaxController);
+   }
+}());
